6603db99c22299edd821f6eb1a22478b53bbaa72
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-07 17:43:38 +0000

    loaded die successfully loops through array

--- make-loaded-die.js
+++ make-loaded-die.js
@@ -1,9 +1,13 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
   /* your code here */
+  let i = 0
 
   return function() {
     /* your code here */
+    let currentRoll = list[i]
+    i ++
+    return currentRoll
   }
 }
 
